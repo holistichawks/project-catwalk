94cdaeefe5cfdd7a1185b7e4f85419559870560d
cleung0112 <cleung0112@gmail.com>
2021-03-22 00:44:45 -0700

    Add comment

--- client/src/components/overview/DefaultImages/Modal/ZoomImage.jsx
+++ client/src/components/overview/DefaultImages/Modal/ZoomImage.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Styles from './Modal.css';
 
-const { Component } = React;
-
-class ZoomImage extends Component {
+class ZoomImage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      backgroundImage: `url(${this.props.src})`,
+      backgroundImage: `url(${props.src})`,
+
+      // For positioning the background image, initially it has no change
       backgroundPosition: '0% 0%',
     };
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
-  handleMouseMove(e) {
+  // Change the position of the background image based on mouse movement
+  handleMouseMove(event) {
     const {
       left, top, width, height,
-    } = e.target.getBoundingClientRect();
-    const x = (e.pageX - left) / width * 100;
-    const y = (e.pageY - top) / height * 100;
-    this.setState({ backgroundPosition: `${x}% ${y}%` });
+    } = event.target.getBoundingClientRect();
+    const newX = (event.pageX - left) / width * 100;
+    const newY = (event.pageY - top) / height * 100;
+    this.setState({ backgroundPosition: `${newX}% ${newY}%` });
   }
 
   render() {
@@ -32,7 +33,7 @@ class ZoomImage extends Component {
         <img
           src={this.props.src}
           id={Styles.ZoomImage}
-          alt="Zoom in product pic"
+          alt="Zoom in product picture"
         />
       </figure>
     );
